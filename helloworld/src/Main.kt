7593e9ca6e81d90447c3d8ae2873e87a02f27539
lmchun <lmchun@sbcglobal.net>
2024-10-05 21:46:53 -0400

    hello world and start tip calculator project

---
+++ helloworld/src/Main.kt
@@ -0,0 +1,43 @@
+fun myFunction(name: String) {
+    println("Hello! $name")
+}
+fun main() {
+    val instagramBio: String? = "artyisbestcat"
+//    if (instagramBio != null) {
+//        println(instagramBio.toUpperCase())
+//    }
+    println(instagramBio?.toUpperCase())
+
+//    myFunction(name= "Jay")
+//    val firstName: String = "Lauren"
+    //var weight: 160
+//    val myVariable: Boolean = false
+//    val examScore = 88
+//    val s1 = "Call me"
+//    val s2 = "Maybe"
+//    val combined = s1 + " " + s2
+
+    //I am a comment What even am I doing here
+//val namesOfBebo = mutableListOf<String>("Chase", "Bebo", "Chasey","Thomasson")
+//    namesOfBebo.add("Arty's Son")
+//   println(namesOfBebo[4])
+
+//    for (i in 1..5){
+//        println(i)
+//    }
+//    for (i in 1 until 5){
+//        println(i)
+//    }
+//    println(firstName)
+//    println(combined)
+//    println(firstName[0])
+//    println(firstName.isEmpty())
+//    println(firstName.length)
+//    println(firstName.substring(2, 4))
+
+//    if (examScore > 70){
+//        println("you passed")
+//    } else{
+//        println("you did not pass")
+//    }
+}
